bd5a9ee1076c309c603deec0e391afdc07655b68
Nathan Larsen <nlarsen@nathan2055.com>
2026-04-06 20:37:55 -0400

    Move out nightfox

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -28,21 +28,7 @@ require('lazy').setup({
 
   require 'kickstart.unbundled-plugins.07-blink-autocomplete',
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'EdenEast/nightfox.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'nightfox'
-    end,
-  },
+  require 'kickstart.unbundled-plugins.08-nightfox',
 
   -- Highlight todo, notes, etc in comments
   {
