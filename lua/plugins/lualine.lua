ca11c154bbeeb5975be84a6be5b3de112112f979
Nathan Larsen <nlarsen@nathan2055.com>
2026-04-06 22:24:22 -0400

    Add lualine and barbar

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+-- lualine
+-- https://github.com/nvim-lualine/lualine.nvim
+-- Status line plugin
+
+---@module 'lazy'
+---@type LazySpec
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' }
+  opts = {
+    theme = 'vscode',
+  },
+}
