0398c33b866306af166f2556e121bdbe04691fd7
Nathan Larsen <nlarsen@nathan2055.com>
2026-04-12 20:06:53 -0400

    Fix Kickstart health checks

--- init.lua
+++ init.lua
@@ -13,9 +13,6 @@ require 'options'
 -- [[ Basic Keymaps ]]
 require 'keymaps'
 
--- [[ Ensure Kickstart health checks are imported ]]
-require 'health'
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
